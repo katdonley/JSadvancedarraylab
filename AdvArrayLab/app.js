4318e763bd4ed016f4f4d940ca4f6010268bf76e
Kat Donley <katdonley@Kats-MBP.lan>
2022-02-28 16:11:17 -0700

    worked through lab functions and almost hit MVP

--- AdvArrayLab/app.js
+++ AdvArrayLab/app.js
@@ -126,33 +126,124 @@ console.log('Mexican Foods: ', mexicanFood)
 //1. Create a function that will return all dishes with the cuisine type of "vegetarian"
 //Filter
 
-function problemOne(){
+function findVegFood(){
 
-    let results;
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Vegetarian"){
+            return true;
+        }
+        else{
+            return false;
+        }})
 
 
     return results;
 }
 
+let vegFood = findVegFood();
+console.log('Vegetarian Foods: ', vegFood)
+
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
+
+function findItalFood(){
+    let results = dishes.filter(function(el){
+        if(el.cuisine === "Italian" && el.servings > 5){
+            return true;
+        }
+        else{
+            return false;
+        }})
+        return results;
+}
+
+let largeItalFood = findItalFood();
+console.log('Italian Foods for Large Groups: ', largeItalFood)
 
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
 
+function findIdEqualToServings(){
+    let results = dishes.filter(function(el){
+        if(el.servings === el.id){
+            return true;
+        }
+        else{
+            return false;
+        }})
+        return results;
+}
+
+let idEqualToServings = findIdEqualToServings();
+console.log('These items id is the same as their serving size: ', idEqualToServings)
+
 //4. Create a function that will return only dishes whose serving count is even.
 //Filter
+
+function findEvenServings(){
+    let results = dishes.filter(function(el){
+        if(el.servings % 2 === 0){
+            return true;
+        }
+        else{
+            return false;
+        }})
+        return results;
+}
+
+let evenServings = findEvenServings();
+console.log('Even Servings: ', evenServings)
 
 //5. Create a function that will return dishes whose ingredients array is "tomato" and "cheese".
 //Filter
 
+function findTomatoCheese(){
+    let results = dishes.filter(function(el){
+        if(el.ingredients.includes("tomato") & el.ingredients.includes("cheese")){
+            return true;
+        }
+        else{
+            return false;
+        }})
+        return results;
+}
+
+
+let tomatoCheese = findTomatoCheese();
+console.log('Items with Tomato and Cheese: ', tomatoCheese)
+
 //6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
 //Map
+
+function findCuisineTypes(){
+    let results = dishes.map(function(el){
+        return el.cuisine;
+    })
+    return results;
+}
+
+let typesOfCuisine = findCuisineTypes()
+console.log(typesOfCuisine)
+
 // BONUS: (come back to this after finishing all)
 //6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
 
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
 //Map 
+
+dishes.sort(function(a, b){return a.cuisine - b.name});
+itemAndCuisineType();
+
+function itemAndCuisineType(){
+    let results = dishes.map(function(el){
+        typeName = el.cuisine + " " + el.name;
+        return el.typeName;
+    })
+    return results;
+}
+
+let cuisineTypeAndName = itemAndCuisineType
+console.log(cuisineTypeAndName)
 
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. So this function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
